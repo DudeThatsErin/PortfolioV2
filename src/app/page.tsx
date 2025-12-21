9a7d057c3eaf70e9ab2e345b08eafb3e9a9bc11e
Erin Skidds <dudethatserin@outlook.com>
2025-12-21 20:34:43 +0000

    josh updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="visually-hidden">Erin Skidds - Full-Stack Engineer</h1>
       <p className="hello">Hello, my name is</p>
       <h2 className="name">Erin Skidds.</h2>
-      <p className="iam">I'm a <strong>Full-Stack Engineer</strong> who works on both front-end and back-end code. I have a passion for bot development and learning how to connect language learning models to those bots.</p>
+      <p className="iam">I'm a <strong>Full-Stack Engineer</strong> who works on both front-end and back-end code. I have a passion for bot/ai development.</p>
 
       <p className="shortabout">I have over 5 years of professional experience in Full-Stack Development. I am most skilled with Back-End Development in Python, PHP, and Back-End JavaScript. I specialize in building bots, learning how to connect language learning models to those bots, and websites.</p>
 
